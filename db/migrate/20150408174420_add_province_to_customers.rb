5d7f75b4ec16432c986acfc716fd039f14346150
Kyle Geske <stungeye@gmail.com>
2015-04-08 12:52:19 -0500

    Customers can now be assigned provinces.

---
+++ db/migrate/20150408174420_add_province_to_customers.rb
@@ -0,0 +1,6 @@
+class AddProvinceToCustomers < ActiveRecord::Migration
+  def change
+    add_reference :customers, :province, index: true
+    add_foreign_key :customers, :provinces
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408171931) do
+ActiveRecord::Schema.define(version: 20150408174420) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "full_name"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20150408171931) do
     t.text     "notes"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "province_id"
+  end
+
+  add_index "customers", ["province_id"], name: "index_customers_on_province_id"
+
+  create_table "provinces", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
